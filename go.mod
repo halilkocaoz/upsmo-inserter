3b48646a9f56e07ad73a5d449e5e4269589ef634
halilkocaoz <halil.i.kocaoz@gmail.com>
2021-08-24 04:49:27 +0300

    add: receiving & printing messages

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/halilkocaoz/upsmo-response-inserter
 
 go 1.16
+
+require github.com/michaelbironneau/asbclient v0.0.0-20190125194550-9c3367d7460e
--- main.go
+++ main.go
@@ -1,5 +1,42 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/michaelbironneau/asbclient"
+)
+
+const (
+	maxMessageCount = 250
+)
+
+var (
+	namespace string = os.Getenv("SERVICE_BUS_NAMESPACE")
+	value     string = os.Getenv("SERVICE_BUS_SHARED_ACCESS_KEY_VALUE")
+
+	messages      [maxMessageCount]string
+	receivedCount int = 0
+)
+
+func main() {
+	sb := asbclient.New(asbclient.Topic, namespace, "RootManageSharedAccessKey", value)
+	sb.SetSubscription("database-inserter")
+
+	for {
+		sbMessage, _ := sb.PeekLockMessage("response-database-inserter", 30)
+
+		if sbMessage != nil {
+			message := string(sbMessage.Body)
+			messages[receivedCount] = message
+			receivedCount++
+			log.Printf("%d: %s", receivedCount, message)
+
+			if receivedCount >= maxMessageCount {
+				receivedCount = 0
+			}
+
+			sb.DeleteMessage(sbMessage)
+		}
+	}
 }
