c1aa0c2a819781d2da1516295f0da6babc6e0058
halilkocaoz <halil.i.kocaoz@gmail.com>
2021-08-24 04:57:50 +0300

    add: inserting received messages to database

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/halilkocaoz/upsmo-response-inserter
 
 go 1.16
 
-require github.com/michaelbironneau/asbclient v0.0.0-20190125194550-9c3367d7460e
+require (
+	github.com/lib/pq v1.10.2
+	github.com/michaelbironneau/asbclient v0.0.0-20190125194550-9c3367d7460e
+)
--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"os"
+	"strings"
+
+	_ "github.com/lib/pq"
 
 	"github.com/michaelbironneau/asbclient"
 )
@@ -12,11 +16,15 @@ const (
 )
 
 var (
+	connstr   string = os.Getenv("AZURE_POSTGRES_CONNSTR")
 	namespace string = os.Getenv("SERVICE_BUS_NAMESPACE")
 	value     string = os.Getenv("SERVICE_BUS_SHARED_ACCESS_KEY_VALUE")
 
 	messages      [maxMessageCount]string
 	receivedCount int = 0
+
+	db *sql.DB
+	tx *sql.Tx
 )
 
 func main() {
@@ -33,6 +41,16 @@ func main() {
 			log.Printf("%d: %s", receivedCount, message)
 
 			if receivedCount >= maxMessageCount {
+				db, _ = sql.Open("postgres", connstr)
+				tx, _ = db.Begin()
+
+				for _, m := range messages {
+					splitted := strings.Split(m, " ")
+					tx.Exec(`INSERT INTO "Responses" ("MonitorID", "StatusCode") VALUES ($1, $2);`, splitted[0], splitted[1])
+				}
+
+				tx.Commit()
+				db.Close()
 				receivedCount = 0
 			}
 
