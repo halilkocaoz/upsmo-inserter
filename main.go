f60f540f5f4a7d011d546c9f7b995787dfd73d23
halilkocaoz <halil.i.kocaoz@gmail.com>
2021-08-25 18:43:59 +0300

    refactor:  add responsemessage struct. update variable name & way of splitting messages

--- main.go
+++ main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 	"os"
 	"strings"
-	"time"
 
 	_ "github.com/lib/pq"
 
@@ -14,55 +12,56 @@ import (
 )
 
 const (
-	maxMessageCount = 250
+	maxMessageCountToCollectBeforeInsert = 1000
 )
+
+type responseMessage struct {
+	monitorid  string
+	statuscode string
+}
 
 var (
 	connStr   string = os.Getenv("AZURE_POSTGRES_CONNSTR")
 	namespace string = os.Getenv("SERVICE_BUS_NAMESPACE")
 	keyValue  string = os.Getenv("SERVICE_BUS_SHARED_ACCESS_KEY_VALUE")
 
-	messages      [maxMessageCount]string
-	receivedCount int = 0
+	responseMessages   [maxMessageCountToCollectBeforeInsert]responseMessage
+	splittedBusMessage []string
 
-	insertingBeginAt int64
-	insertingEndAt   int64
+	receivedCount int = 0
 
 	db *sql.DB
 	tx *sql.Tx
 )
 
 func main() {
-	fmt.Printf("%s\n%s\n%s\n", connStr, namespace, keyValue)
 	sb := asbclient.New(asbclient.Topic, namespace, "RootManageSharedAccessKey", keyValue)
 	sb.SetSubscription("database-inserter")
-
 	for {
-		sbMessage, _ := sb.PeekLockMessage("response-database-inserter", 0)
-		if sbMessage != nil {
-			message := string(sbMessage.Body)
-			messages[receivedCount] = message
+		busMessage, _ := sb.PeekLockMessage("response-database-inserter", 0)
+		if busMessage != nil {
+			splittedBusMessage = strings.Split(string(busMessage.Body), " ")
+			responseMessages[receivedCount].monitorid = splittedBusMessage[0]
+			responseMessages[receivedCount].statuscode = splittedBusMessage[1]
 			receivedCount++
-			log.Printf("%d: %s", receivedCount, message)
 
-			if receivedCount >= maxMessageCount {
+			log.Printf("%d: %s", receivedCount, splittedBusMessage)
+
+			if receivedCount >= maxMessageCountToCollectBeforeInsert {
 				log.Println("inserting")
-				insertingBeginAt = time.Now().Unix()
 				db, _ = sql.Open("postgres", connStr)
 				tx, _ = db.Begin()
 
-				for _, m := range messages {
-					splitted := strings.Split(m, " ")
-					tx.Exec(`INSERT INTO "Responses" ("MonitorID", "StatusCode") VALUES ($1, $2);`, splitted[0], splitted[1])
+				for _, message := range responseMessages {
+					tx.Exec(`INSERT INTO "Responses" ("MonitorID", "StatusCode") VALUES ($1, $2);`, message.monitorid, message.statuscode)
 				}
 
 				tx.Commit()
 				db.Close()
-				insertingEndAt = time.Now().Unix()
-				log.Printf("elapsed sec: %d", insertingEndAt-insertingBeginAt)
 				receivedCount = 0
 			}
-			sb.DeleteMessage(sbMessage)
+
+			sb.DeleteMessage(busMessage)
 		}
 	}
 }
